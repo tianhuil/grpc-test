7559e6e5a0a3abfd586b8cc93aa0d3edb973bcc2
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-05-28 23:20:36 -0400

    implement more functions

--- python/greeter_client.py
+++ python/greeter_client.py
@@ -13,6 +13,8 @@ def run():
   stub = helloworld_pb2_grpc.GreeterStub(channel)
   response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
   print("Greeter client received: " + response.message)
+  response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
+  print("Greeter client received: " + response.message)
 
 
 if __name__ == '__main__':
